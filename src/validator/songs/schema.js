8def9f6a66b3d6d2464c5f23ae9d7285e94dcd57
Erlangga Juni Saputra <erlanggajunisaputra@gmail.com>
2023-07-12 06:03:08 +0700

    memperbaiki validator schema

--- src/validator/songs/schema.js
+++ src/validator/songs/schema.js
@@ -1,9 +1,9 @@
 const Joi = require('joi');
 
 const SongsPayloadSchema = Joi.object({
-  title: Joi.string().required(),
+  title: Joi.string().max(255).required(),
   year: Joi.number().required(),
-  genre: Joi.string().required(),
+  genre: Joi.string().max(255).required(),
   performer: Joi.string().required(),
   duration: Joi.number(),
   albumId: Joi.string(),
