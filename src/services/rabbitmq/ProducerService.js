660346327792fe77bd04c0de9837a20c2a477471
Erlangga Juni Saputra <erlanggajunisaputra@gmail.com>
2023-07-18 06:52:27 +0700

    fix incorrect sendMessage queue

--- src/services/rabbitmq/ProducerService.js
+++ src/services/rabbitmq/ProducerService.js
@@ -1,7 +1,7 @@
 const amqp = require('amqplib');
 
 const ProducerService = {
-  sendMessage: async (message, queue) => {
+  sendMessage: async (queue, message) => {
     const connection = await amqp.connect(process.env.RABBITMQ_SERVER);
     const channel = await connection.createChannel();
     await channel.assertQueue(queue, {
